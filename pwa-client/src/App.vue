46048fc5b03ab57d7653caef4ed81183d7bd093d
s-apergis <simos.apergis@allwyn-lotterysolutions.com>
2025-12-16 19:13:35 +0200

    hide notification bell

--- pwa-client/src/App.vue
+++ pwa-client/src/App.vue
@@ -10,8 +10,8 @@
           <RouterLink to="/upload" class="hover:text-primary-600">Σάρωση</RouterLink>
           <RouterLink to="/suppliers" class="hover:text-primary-600">Προμηθευτές</RouterLink>
 
-          <!-- Notifications -->
-          <RouterLink to="/notifications" class="relative hover:text-primary-600">
+          <!-- Notifications (hidden until FCM is enabled) -->
+          <!-- <RouterLink to="/notifications" class="relative hover:text-primary-600">
             <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
             </svg>
@@ -21,7 +21,7 @@
             >
               {{ unreadCount > 99 ? '99+' : unreadCount }}
             </span>
-          </RouterLink>
+          </RouterLink> -->
 
           <!-- User menu -->
           <div class="relative ml-2">
